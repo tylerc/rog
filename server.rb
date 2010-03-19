bb6791e2da32ede26424324f0ced8a2c339363c3
Tyler Church <tylertrain@yahoo.com>
2010-03-18 22:53:10 -0700

    The player cannot leave the confines of his room

--- server.rb
+++ server.rb
@@ -5,6 +5,9 @@ require 'digest/sha1'
 $salt = rand
 $ids = {}
 $num = 0
+$rooms = {0 => {:width => 320, :height => 320}}
+$player_width = 20
+$player_height = 20
 
 helpers do
 	def cleanup_timeouts
@@ -23,7 +26,7 @@ end
 
 get '/add_player/?' do
 	id = Digest::SHA1.hexdigest(($num + $salt).to_s)
-	$ids[id] = {:x => rand(280), :y => rand(280), :color => [params[:r].to_i,params[:g].to_i,params[:b].to_i], :last_conn => Time.now, :name => params[:name], :num => $num}
+	$ids[id] = {:x => rand(280), :y => rand(280), :color => [params[:r].to_i,params[:g].to_i,params[:b].to_i], :last_conn => Time.now, :name => params[:name], :num => $num, :room => 0}
 	$num += 1
 	id
 end
@@ -55,6 +58,9 @@ end
 get '/set_x/:x' do
 	unless params[:x] == nil
 		$ids[params[:id]][:x] = params[:x].to_i
+		$ids[params[:id]][:x] = 0 if params[:x].to_i < 0
+		room = $ids[params[:id]][:room]
+		$ids[params[:id]][:x] = $rooms[room][:width]-$player_width if params[:x].to_i > $rooms[room][:width]-$player_width
 		return $ids[params[:id]][:x].to_s
 	end
 end
@@ -62,6 +68,9 @@ end
 get '/set_y/:y' do
 	unless params[:y] == nil
 		$ids[params[:id]][:y] = params[:y].to_i
+		$ids[params[:id]][:y] = 0 if params[:y].to_i < 0
+		room = $ids[params[:id]][:room]
+		$ids[params[:id]][:y] = $rooms[room][:height]-$player_height if params[:y].to_i > $rooms[room][:height]-$player_height
 		return $ids[params[:id]][:y].to_s
 	end
 end
