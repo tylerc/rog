6c4575fae20594cf588373ba03d2b908b2752708
Tyler Church <tylertrain@yahoo.com>
2010-04-05 18:57:08 -0700

    Added to_i's to our randomly generated x's and y's in room.rb, since sometimes they make decimals. Decimals are bad!

--- room.rb
+++ room.rb
@@ -59,8 +59,8 @@ class Rooms
 		
 		case side
 			when :top
-				x = rand(room1[:width]-100)
-				x2 = rand(room2[:width]-100)
+				x = rand(room1[:width]-100).to_i
+				x2 = rand(room2[:width]-100).to_i
 				door1 = [x,0,x+100,2]
 				door2 = [x2,room2[:height]-2,x2+100,room2[:height]]
 				spawn1 = [door2[0]+50-10,room2[:height]-30]
@@ -70,8 +70,8 @@ class Rooms
 				room2[:doors][room1[:num]] = [door2,spawn2]
 				room2[:has_doors_on] += [:bottom]
 			when :right
-				y = rand(room1[:height]-100)
-				y2 = rand(room2[:height]-100)
+				y = rand(room1[:height]-100).to_i
+				y2 = rand(room2[:height]-100).to_i
 				door1 = [room1[:width]-2,y,room1[:width],y+100]
 				door2 = [0,y2,2,y2+100]
 				spawn1 = [10,door2[1]+50-10]
@@ -81,8 +81,8 @@ class Rooms
 				room2[:doors][room1[:num]] = [door2,spawn2]
 				room2[:has_doors_on] += [:left]
 			when :bottom
-				x = rand(room1[:width]-100)
-				x2 = rand(room2[:width]-100)
+				x = rand(room1[:width]-100).to_i
+				x2 = rand(room2[:width]-100).to_i
 				door1 = [x,room1[:height]-2,x+100,room1[:height]]
 				door2 = [x2,0,x2+100,2]
 				spawn1 = [door2[0]+50-10,10]
@@ -92,8 +92,8 @@ class Rooms
 				room2[:doors][room1[:num]] = [door2,spawn2]
 				room2[:has_doors_on] += [:top]
 			when :left
-				y = rand(room1[:height]-100)
-				y2 = rand(room2[:height]-100)
+				y = rand(room1[:height]-100).to_i
+				y2 = rand(room2[:height]-100).to_i
 				door1 = [0,y,2,y+100]
 				door2 = [room2[:width]-2,y2,room2[:width],y2+100]
 				spawn1 = [room2[:width]-30,door2[1]+50-10]
